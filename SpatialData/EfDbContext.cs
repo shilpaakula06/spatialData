0d9ab3a0220776797457a2dc4da2385f0ca00850
shilpaakula06 <shilpa.akula@ncompasbusiness.com>
2023-09-28 14:11:06 +0530

    .

---
+++ SpatialData/EfDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpatialData
+{
+    public class EfDbContext : System.Data.Entity.DbContext
+    {
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //TODO Extract connection string to a secret
+        //    optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCodeFirstDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //}
+        public System.Data.Entity.DbSet<Item> Items { get; set; }
+    }
+}
--- SpatialData/Program.cs
+++ SpatialData/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!yesyes");
+using System.Data.Entity;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello World!");
+
+        using (var context = new SpatialData.EfDbContext())
+        {
+            var Item = new SpatialData.Item()
+            {
+                Name = "Ron Palido",
+                Description = "Drink",
+                Expiration = DateTime.Now.AddYears(1)
+
+            };
+            Console.WriteLine($"Item NOT saved -> Id {Item.id} {Item.Name} {Item.Expiration}");
+
+            //context.Add(Item);
+            context.SaveChanges();
+
+            Console.WriteLine($"Item saved -> Id {Item.id} {Item.Name} {Item.Expiration}");
+            Console.ReadKey();
+        }
+    }
+}
